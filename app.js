c00dc203cf80ee2400b8cb3c5ca3612e226dabad
Winnie Mei <wmei91@gmail.com>
2015-06-25 18:23:30 -0400

    wrapped everything in if

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 $(function(){
 
-
-
 var a1 = $("#a1");
 var b1 = $("#b1");
 var c1 = $("#c1");
@@ -59,119 +57,170 @@ var board = [
 			[a1, b1, c1, d1, e1, f1, g1],
 			];
 
+// ------------------------------------------------------------
+
+turn = 1;
+
+if(turn % 2 !== 0) {
+	var columnA = $("#a");
+
+	var dropChipA = function(){
+		for(var i = 5; i <= 5; i--){
+			if(board[i][0].hasClass("empty")){
+				return (board[i][0].removeClass("empty").addClass("empty1"));
+		}
+		}
+	};
+
+	var columnB = $("#b");
+
+	var dropChipB = function(){
+		for(var i = 5; i <= 5; i--){
+			if(board[i][1].hasClass("empty")){
+				return (board[i][1].removeClass("empty").addClass("empty1"));
+		}
+
+		}
+	};
+
+	var columnC = $("#c");
+
+	var dropChipC = function(){
+		for(var i = 5; i <= 5; i--){
+			if(board[i][2].hasClass("empty")){
+				return (board[i][2].removeClass("empty").addClass("empty1"));
+		}
+
+		}
+	};
+
+	var columnD = $("#d")
+
+	var dropChipD = function(){
+		for(var i = 5; i <= 5; i--){
+			if(board[i][3].hasClass("empty")){
+				return (board[i][3].removeClass("empty").addClass("empty1"));
+		}
+
+		}
+	};
+
+	var columnE = $("#e")
+
+	var dropChipE = function(){
+		for(var i = 5; i <= 5; i--){
+			if(board[i][4].hasClass("empty")){
+				return (board[i][4].removeClass("empty").addClass("empty1"));
+		}
+
+		}
+	};
+
+	var columnF = $("#f")
+
+	var dropChipF = function(){
+		for(var i = 5; i <= 5; i--){
+			if(board[i][5].hasClass("empty")){
+				return (board[i][5].removeClass("empty").addClass("empty1"));
+		}
+
+		}
+	};
+
+	var columnG = $("#g")
+
+	var dropChipG = function(){
+		for(var i = 5; i <= 5; i--){
+			if(board[i][6].hasClass("empty")){
+				return (board[i][6].removeClass("empty").addClass("empty1"));
+			}
+
+		}
+	};
+	turn = 2
+}
+
+else if (turn == 2) {
+
+	console.log("LOL");
+}
+	// var columnA = $("#a");
+
+	// var dropChipA = function(){
+	// 	for(var i = 5; i <= 5; i--){
+	// 		if(board[i][0].hasClass("empty")){
+	// 			return (board[i][0].removeClass("empty").addClass("empty1"));
+	// 	}
+	// 	}
+	// };
+
+	// var columnB = $("#b");
+
+	// var dropChipB = function(){
+	// 	for(var i = 5; i <= 5; i--){
+	// 		if(board[i][1].hasClass("empty")){
+	// 			return (board[i][1].removeClass("empty").addClass("empty1"));
+	// 	}
+
+	// 	}
+	// };
+
+	// var columnC = $("#c");
+
+	// var dropChipC = function(){
+	// 	for(var i = 5; i <= 5; i--){
+	// 		if(board[i][2].hasClass("empty")){
+	// 			return (board[i][2].removeClass("empty").addClass("empty1"));
+	// 	}
+
+	// 	}
+	// };
+
+	// var columnD = $("#d")
+
+	// var dropChipD = function(){
+	// 	for(var i = 5; i <= 5; i--){
+	// 		if(board[i][3].hasClass("empty")){
+	// 			return (board[i][3].removeClass("empty").addClass("empty1"));
+	// 	}
+
+	// 	}
+	// };
+
+	// var columnE = $("#e")
+
+	// var dropChipE = function(){
+	// 	for(var i = 5; i <= 5; i--){
+	// 		if(board[i][4].hasClass("empty")){
+	// 			return (board[i][4].removeClass("empty").addClass("empty1"));
+	// 	}
+
+	// 	}
+	// };
+
+	// var columnF = $("#f")
+
+	// var dropChipF = function(){
+	// 	for(var i = 5; i <= 5; i--){
+	// 		if(board[i][5].hasClass("empty")){
+	// 			return (board[i][5].removeClass("empty").addClass("empty1"));
+	// 	}
+
+	// 	}
+	// };
+
+	// var columnG = $("#g")
+
+	// var dropChipG = function(){
+	// 	for(var i = 5; i <= 5; i--){
+	// 		if(board[i][6].hasClass("empty")){
+	// 			return (board[i][6].removeClass("empty").addClass("empty1"));
+	// 		}
+
+	// 	}
+	// };
 
 
-
-
-	// var turn = 0;
-	// var tttClick = function(element) {
-	// 	if (element.target.nodeName === "BUTTON") {
-	// 	  if (element.target.innerHTML.length === 0) {	
-
-	// 		if (turn === 0) {
-	// 			element.target.innerText = 'x';
-	// 			turn = 1;
-	// 		} else if (turn === 1) {
-	// 			element.target.innerText = 'o';
-	// 			turn = 0;
-	// 		};
-	// 	  };
-	// 	};
-
-
-// var turn = 0;
-// // var connectFourClick = function(){
-// 	if (turn === 0){
-// 		$(document).click(function(){
-// 			return turn === 1
-// 		}
-// 	else if (turn === 1){
-// 		$(document).click(function(){
-// 			return turn === 0
-// 		}
-// 	}
-// }
-
-
-// 	}
-// }
-
-
-var columnA = $("#a");
-
-var dropChipA = function(){
-	for(var i = 5; i <= 5; i--){
-		if(board[i][0].hasClass("empty")){
-			return (board[i][0].removeClass("empty").addClass("empty1"));
-	}
-	}
-};
-
-var columnB = $("#b");
-
-var dropChipB = function(){
-	for(var i = 5; i <= 5; i--){
-		if(board[i][1].hasClass("empty")){
-			return (board[i][1].removeClass("empty").addClass("empty1"));
-	}
-
-	}
-};
-
-var columnC = $("#c");
-
-var dropChipC = function(){
-	for(var i = 5; i <= 5; i--){
-		if(board[i][2].hasClass("empty")){
-			return (board[i][2].removeClass("empty").addClass("empty1"));
-	}
-
-	}
-};
-
-var columnD = $("#d")
-
-var dropChipD = function(){
-	for(var i = 5; i <= 5; i--){
-		if(board[i][3].hasClass("empty")){
-			return (board[i][3].removeClass("empty").addClass("empty1"));
-	}
-
-	}
-};
-
-var columnE = $("#e")
-
-var dropChipE = function(){
-	for(var i = 5; i <= 5; i--){
-		if(board[i][4].hasClass("empty")){
-			return (board[i][4].removeClass("empty").addClass("empty1"));
-	}
-
-	}
-};
-
-var columnF = $("#f")
-
-var dropChipF = function(){
-	for(var i = 5; i <= 5; i--){
-		if(board[i][5].hasClass("empty")){
-			return (board[i][5].removeClass("empty").addClass("empty1"));
-	}
-
-	}
-};
-
-var columnG = $("#g")
-
-var dropChipG = function(){
-	for(var i = 5; i <= 5; i--){
-		if(board[i][6].hasClass("empty")){
-			return (board[i][6].removeClass("empty").addClass("empty1"));
-	}
-
-	}
-};
 
 columnA.click(dropChipA);
 columnB.click(dropChipB);
